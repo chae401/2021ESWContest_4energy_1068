546bc4663dfdb6746adb01f4d47bd624a55b9d23
kyounghwan kim <79563142+KyongHwan-Kim@users.noreply.github.com>
2021-09-28 03:01:48 +0900

    update close function

--- IoT_Pipe_code/Action_pi/nCube-Thyme-Nodejs/led_control.py
+++ IoT_Pipe_code/Action_pi/nCube-Thyme-Nodejs/led_control.py
@@ -69,3 +69,19 @@ def normal():
     GPIO.output(upper_green_led, True)
     GPIO.output(under_red_led, False)
     GPIO.output(under_green_led, True)
+def close():
+    upper_green_led = 14
+    upper_red_led = 15
+    under_green_led = 16
+    under_red_led = 17
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(upper_green_led, GPIO.OUT)
+    GPIO.setup(upper_red_led, GPIO.OUT)
+    GPIO.setup(under_green_led, GPIO.OUT)
+    GPIO.setup(under_red_led, GPIO.OUT)
+
+    GPIO.output(upper_red_led, True)
+    GPIO.output(upper_green_led, False)
+    GPIO.output(under_red_led, True)
+    GPIO.output(under_green_led, False)
